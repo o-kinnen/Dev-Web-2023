56e410a3b05ac46052b21fe5025528d2b4d3f6cd
Antho3004 <anthony.iv14@gmail.com>
2023-04-26 00:23:05 +0200

    creation de la page connexion

---
+++ frontend/src/views/Connexion.vue
@@ -0,0 +1,29 @@
+<template>
+    <br><br><br><br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">Left</div>
+            <div class="col-md-6">
+                <form>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            placeholder="Enter email" />
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Connexion</button>
+                </form>
+            </div>
+            <div class="col-md-3">Right</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ConnexionSotrexco",
+};
+</script>
+
+<style scoped></style>
