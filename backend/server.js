8ce44424504d0d1937915d0704b5012cb232ecfa
Olivia Kinnen <o.kinnen@students.ephec.be>
2023-05-03 13:37:40 +0200

    Update

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(3000, () =>{
-    console.log('Le serveur écoute sur le port 3100!');
+    console.log('Le serveur écoute sur le port 3000!');
 });
 
 // Route "produit" pour récupérer tous les produits
