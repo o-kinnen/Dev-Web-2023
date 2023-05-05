30feacb06208fd0b3f95d6df31ebc2fe2b0ce643
Tatiana Saragossi <t.saragossi@students.ephec.be>
2023-05-05 12:56:52 +0200

    ajout de la fonctionnalité modale pour les clients

--- frontend/src/components/ModaleClients.vue
+++ frontend/src/components/ModaleClients.vue
@@ -2,12 +2,8 @@
 <template>
 
   <div class="bloc-modale">
-
-    <div class="overlay"></div>
-
-    <div class="modale-card"></div>
-    <div class="btn-modale btn btn-danger">
-      <table class="clients-table">
+    <div class="modal-wrapper">
+      <table class="modale-clients-table">
         <thead>
         <tr>
           <th>Nom de la société</th>
@@ -63,4 +59,31 @@ export default {
 </script>
 
 
-  <style></style>
+<style>
+  .bloc-modale{
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity 0.3s ease;
+  }
+  .modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+    width : 100px
+  }
+  .modale-clients-table{
+    width: 1000px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+</style>
