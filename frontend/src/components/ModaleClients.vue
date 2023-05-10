e9ecabbe2698133a5015f7ea739d726d46cd4cd4
Tatiana Saragossi <t.saragossi@students.ephec.be>
2023-05-10 18:23:01 +0200

    ajout de bootstrap et cypress

--- frontend/src/components/ModaleClients.vue
+++ frontend/src/components/ModaleClients.vue
@@ -1,37 +1,51 @@
 
 <template>
 
-  <div class="bloc-modale">
-    <div class="modal-wrapper">
-      <table class="modale-clients-table">
-        <thead>
-        <tr>
-          <th>Nom de la société</th>
-          <th>Personne de contact</th>
-          <th>Fonction</th>
-          <th>numéro de téléphone</th>
-          <th>adresse e-mail</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>{{client.nom_societe}}</td>
-          <td>{{client.prenom}} {{client.nom}}</td>
-          <td>{{client.fonction}}</td>
-          <td><a href="tel:{{client.telephone}}">{{client.telephone}}</a></td>
-          <td><a href="mailto:{{client.mail_client}}">{{client.mail_client}}</a></td>
-        </tr>
-        </tbody>
-      </table>
-      <button color="red" @click="$emit('fermer')">fermer</button>
+  <!-- Modal -->
+  <div class="modal fade" id="modal" aria-labelledby="exampleModalLabel">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="bloc-modale">
+            <div class="modal-wrapper">
+              <table class="modale-clients-table">
+                <thead>
+                <tr>
+                  <th>Nom de la société</th>
+                  <th>Personne de contact</th>
+                  <th>Fonction</th>
+                  <th>numéro de téléphone</th>
+                  <th>adresse e-mail</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>{{client.nom_societe}}</td>
+                  <td>{{client.prenom}} {{client.nom}}</td>
+                  <td>{{client.fonction}}</td>
+                  <td><a href="tel:{{client.telephone}}">{{client.telephone}}</a></td>
+                  <td><a href="mailto:{{client.mail_client}}">{{client.mail_client}}</a></td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" color="red" data-bs-dismiss="modal">fermer</button>
+        </div>
+      </div>
     </div>
   </div>
-
 </template>
 
 
 <script>
-
+import {Modal} from "bootstrap"
 import axios from "axios";
 export default {
   name: "ModaleClients",
@@ -54,12 +68,19 @@ export default {
   },
   mounted() {
     this.getClient();
+    const modaleElem = document.getElementById("modal");
+    const modale = new Modal(modaleElem);
+    modaleElem.addEventListener("hidden.bs.modal",  ()=>{
+      this.$emit("fermer")
+    });
+    modale.show();
   },
 }
 </script>
 
 
 <style>
+/*
   .bloc-modale{
     position: fixed;
     z-index: 9998;
@@ -85,5 +106,5 @@ export default {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
     transition: all 0.3s ease;
     font-family: Helvetica, Arial, sans-serif;
-  }
+  }*/
 </style>
