fd6ebf5d74bbef188ce7ec2a4b6b075cd0438167
Antho3004 <anthony.iv14@gmail.com>
2023-05-19 15:48:43 +0200

    Correction des config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,7 @@
-module.export = {
-    preset: '@vue/cli-plugin-unit-jest'
+module.exports = {
+    preset: '@vue/cli-plugin-unit-jest',
+    transformIgnorePatterns: ["node_modules/(?!axios)/"],
+    transform: {
+        ".*\\.(vue)$": "@vue/vue3-jest",
+    }
 }
