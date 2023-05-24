7bbd0b6dd8676983df3794065d05f62767db10ee
Arthur Kalinowska <a.kalinowska@students.ephec.be>
2023-05-24 23:40:49 +0200

    Update US Perso + Ajout formulaire Commande

---
+++ frontend/src/views/Commande.vue
@@ -0,0 +1,55 @@
+<template>
+  <br><br><br><br>
+  <p>Vous voulez commander un service/produit ?</p>
+  <form>
+    <p>
+      <label for="mail">Mail : </label>
+      <input type="text" v-model="mail" name="mail">
+    </p>
+    <p>
+      <label for="tel">Tel : </label>
+      <input type="text" v-model="tel" name="tel">
+    </p>
+    <p>
+      <label for="type">Objet de la commande</label>
+      <input type="radio" v-model="type" value="Produit">
+      <label>Produit</label>
+      <input type="radio" v-model="type" value="Service">
+      <label>Service</label>
+    </p>
+    <p>
+      <label for="nom">Nom : </label>
+      <input type="text" v-model="nom" name="nom">
+    </p>
+    <p>
+      <label for="description">Description : </label>
+      <input type="text" name="description">
+    </p>
+    <p>
+      <input type="submit" value="Envoyer">
+    </p>
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: "CommandeTest",
+  data() {
+    return {
+      type: '',
+      tel: '',
+      mail: '',
+      nom: ''
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+form{
+  text-align: center;
+}
+
+</style>
